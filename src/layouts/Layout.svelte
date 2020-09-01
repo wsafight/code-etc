c6be4daa5b78986c6e1549963b2d457dfdb7d123
王世安 <984292420@qq.com>
2020-09-01 15:29:13 +0800

    feat: 添加滚动指示器

--- src/layouts/Layout.svelte
+++ src/layouts/Layout.svelte
@@ -6,6 +6,29 @@
   :global(h1) {
     font-style: italic;
   }
+
+  .main {
+    position: relative;
+  }
+
+  .indicator {
+    position: absolute;
+    top: 0; right: 0; left: 0; bottom: 0;
+    background: linear-gradient(to right top, teal 50%, transparent 50%) no-repeat;
+    background-size: 100% calc(100% - 100vh);
+    z-index: 1;
+    pointer-events: none;
+    mix-blend-mode: darken;
+  }
+
+  .indicator::after {
+    content: '';
+    position: fixed;
+    top: 5px; bottom: 0; right: 0; left: 0;
+    background: #fff;
+    z-index: 1;
+  }
+
   .container {
     max-width: 900px;
     margin: 0 auto;
@@ -22,6 +45,11 @@
   <link rel="stylesheet" href="{settings.locations.assets.replace('./public', '')}style.css" />
   <link rel="stylesheet" href="https://unpkg.com/balloon-css/balloon.min.css" />
 </svelte:head>
-<div class="container">
-  {@html routeHTML}
+
+<div class="main">
+  <div class="indicator"></div>
+  <main class="container">
+    {@html routeHTML}
+  </main>
 </div>
+

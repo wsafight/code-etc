2ac150138328f06e2d116539444f7c479101739e
王世安 <984292420@qq.com>
2020-09-10 00:34:39 +0800

    添加问题模块

---
+++ src/routes/home/Questions.svelte
@@ -0,0 +1,24 @@
+<script>
+  const questions = [
+    {
+      q: '为何要编写该实践?',
+      a: ['之所以编写该实践，是因为我相信我可以凭借该项目成为一个更好的开发者，并且可以帮助其他人']
+    },
+    {
+      q: '我该如何使用它?',
+      a: ['之所以编写该实践，是因为我相信我可以凭借该项目成为一个更好的开发者，并且可以帮助其他人']
+    },
+  ]
+</script>
+
+<article>
+    <h2>问题</h2>
+    {#each questions as question}
+        <details>
+            <summary>{question.q}</summary>
+            {#each question.a as answer}
+                <p>{answer}</p>
+            {/each}
+        </details>
+    {/each}
+</article>

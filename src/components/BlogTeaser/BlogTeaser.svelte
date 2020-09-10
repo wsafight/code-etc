7ca0bf3a21df92d1e723031511382ff1bd26cfa6
王世安 <984292420@qq.com>
2020-09-10 20:13:32 +0800

    代码修改

--- src/components/BlogTeaser/BlogTeaser.svelte
+++ src/components/BlogTeaser/BlogTeaser.svelte
@@ -6,9 +6,8 @@
   <h3>{blog.title}</h3>
   <p>{blog.desc}</p>
   <p>
-    <a href={blog.url} target="_blank" >
+    <a href={blog.url} target="_blank" rel="noopener noreferrer" >
       <em>查看 ↗</em>
     </a>
   </p>
-
 </aside>

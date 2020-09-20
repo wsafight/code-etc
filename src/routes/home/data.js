00ce01f080eb1dd3e83b8859c91660d1559d48ee
wsa <984292420>
2020-09-20 15:12:39 +0800

    feat: 添加前言

--- src/routes/home/data.js
+++ src/routes/home/data.js
@@ -1,15 +1,7 @@
 const {hookInterface} = require('@elderjs/elderjs');
-const componentRules = require('../../data/component-rules')
-const cssRules = require('../../data/css-rules')
-const htmlRules = require('../../data/html-rules')
-const jsRules = require('../../data/js-rules')
 
 module.exports = ({request, data}) => {
   data.hookInterface = hookInterface;
-  data.componentRules = componentRules;
-  data.cssRules = cssRules;
-  data.htmlRules = htmlRules;
-  data.jsRules = jsRules;
 
   return data;
 };
